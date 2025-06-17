cf35156ad1da1f6ec558b36cfb3a9d48c68af273
AOIROSERVER <aoiroserver@gmail.com>
2025-06-18 00:51:34 +0900

    chore: Netlify用にnext export対応

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
   description: "鉄道運行情報アプリ",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="ja">
       <body className={inter.className}>
