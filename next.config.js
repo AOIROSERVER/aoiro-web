943df23eb83e4dab361c46361b06ab24d4ef14a3
AOIROSERVER <aoiroserver@gmail.com>
2025-07-03 07:09:21 +0900

    chore: Netlify用にnext export対応

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        module: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
